b85611f0dadecaa69559905e4118b83b26c6974e
parisvennis <parisvennis@gmail.com>
2020-10-08 00:11:21 +0000

    update

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,12 @@
 
 import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
+import "breathecode-dom"; //DOM override to make JS easier to use
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let num = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let ranSuit = ["&#9824", "&#9829", "&#9827", "&#9830"];
+
+  console.log(ranSuit[Math.floor(Math.random() * ranSuit.length + 1)]);
 };
